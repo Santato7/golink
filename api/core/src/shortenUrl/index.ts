9bc51038a25c702902eb5ff9414b0baccda87794
Santato <santatolucas7@gmail.com>
2023-09-13 23:25:20 -0300

    bohr deploy

--- api/core/src/shortenUrl/index.ts
+++ api/core/src/shortenUrl/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
 
-let userUrl: string;
-
 const shortenUrl = (req: express.Request, res: express.Response): any => {
+  let userUrl: string;
   userUrl = req.body.userUrl;
+
+  userUrl = verifyUrl(userUrl);
   res.send(req.body.userUrl);
+};
+
+const verifyUrl = (_url: string) => {
+  if (!/^(https?:\/\/)/i.test(_url)) {
+    _url = `https://${_url}`;
+  }
+
+  return _url;
 };
 
 export default shortenUrl;
