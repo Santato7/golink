dd6781283627f7a2e3c1ece67c8d094f3b80f31c
santato <santatolucas7@gmail.com>
2023-09-14 03:15:12 -0300

    small fix

--- api/core/src/db/index.ts
+++ api/core/src/db/index.ts
@@ -1,5 +1,5 @@
 const mysql = require("mysql2/promise");
-require("dotenv").config();
+import "dotenv/config";
 
 export default async function getConnection() {
   return mysql.createConnection(process.env.DATABASE_URL);
